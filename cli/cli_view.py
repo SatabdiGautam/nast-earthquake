a2fc52db1679018a4cb2abfb1c54c338c5021c21
Satabdi Gautam <satabdigautam@gmail.com>
2025-07-22 07:07:26 +0545

    refactor

--- cli/cli_view.py
+++ cli/cli_view.py
@@ -1,2 +1,10 @@
-name = "Nast earthquake visualization in Cli"
-print (name)
+def main():
+    choice = 0 
+    while choice !=1 :
+        print()
+        print("***** CHOOSE TASK *****")
+        print("1)")
+        choice = int(input())
+
+if __name__ == "__main__":
+    main()    
